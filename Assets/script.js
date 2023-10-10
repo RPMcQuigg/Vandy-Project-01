aaf9e58cb7bcd557de55d6795a9db7112c0ebc86
JulieABowers <142258129+JulieABowers@users.noreply.github.com>
2023-10-10 14:48:34 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -136,7 +136,8 @@ function displayForecast(data) {
 }
 
 
-var getEventsSearch = async function (city) {
+var getEventsSearch = async function (city) 
+{
     const eventsAPIKey = "KRxYIgVel9CyKuLI2MUA6RETp7Q3HXxl";
     const eventsAPIBaseUrl = "https://app.ticketmaster.com/discovery/v2/events.json";
     //URL for api cannot have spaces so replace any spaces in the city name with underscores
@@ -145,208 +146,169 @@ var getEventsSearch = async function (city) {
     var eventSearchParams = `?apikey=${eventsAPIKey}&size=60&sort=date,name,asc`;
     eventSearchParams = !cityMod == "" ? eventSearchParams + `&city=${cityMod}` : eventSearchParams;
     //append the keyword(s) to the url if they are present
-    var evtKeyword = document.getElementById("destinationInput").value.trim();
+    var evtKeyword = document.getElementById("eventsKeywordInput").value.trim();
     eventSearchParams = !evtKeyword == "" ? eventSearchParams + `&keyword=${evtKeyword}` : eventSearchParams;
     var apiUrl = eventsAPIBaseUrl + eventSearchParams;
-    console.log(apiUrl);
-
-    try {
-        //Dynamically add events to the list. The function takes: tag type, image source (if applicable), id, id suffix, 
-        //mouse over action, mouse out action, cursor style, class, and text content.
-        //addEventList(tagType, imgSrc, id, idSuffix, mouseOver, mouseOut, cursorStyle, classType, contentVal)
-
-        try {
-            //Dynamically add events to the list. The function takes: parent div, tag type, image source (if applicable), id, 
-            //id suffix, class, and text content.
-            //addEventList(parentDiv, tagType, imgSrc, id, idSuffix, classType, contentVal)
-            const res = await fetch(apiUrl);
-            const data = await res.json();
-            //max image size to display
-            var imgWidth = 205;
-            var imgHeight = 115;
-            var tagP = "<p>";
-
-            if (window.screen.height <= 900) {
-                //min image size to display
-                imgWidth = 100;
-                imgHeight = 56;
-            }
-
-            //Skip adding html elements if no event returned
-            if (data.page.totalPages >> 0) {
-                for (var i = 0; i < data._embedded.events.length; i++) {
-                    var newBlockRow = $("<div>")
-
-                    newBlockRow.addClass(`grid grid-cols-11 grid-rows-3 grid-flow-col gap-2 border-2 border-solid border-black`);
-                    //add the event image. multiple images available so loop through to get correct size
-                    for (var m = 0; m < data._embedded.events[i].images.length; m++) {
-                        if (data._embedded.events[i].images[m].width == imgWidth && data._embedded.events[i].images[m].height == imgHeight) {
-                            //addEventList(newBlockRow, '<img>', data._embedded.events[i].images[m].url, "row-span-3 col-span-1 h-auto max-w-full max-h-full");
-                            addEventList(newBlockRow, '<div>', data._embedded.events[i].images[m].url, "row-span-3 col-span-1 max-w-full", "backgroundimage", "");
-                            break;
-                        }
-                    }
-
-                    //add the event name
-                    addEventList(newLinkRow, tagP, "", "col-span-8", id, "N", data._embedded.events[i].name);
-
-                    //add the event date
-                    var localEventDate = dayjs(data._embedded.events[i].dates.start.localDate).format("MM/DD/YYYY");
-                    var localEventTime = data._embedded.events[i].dates.start.localTime;
-                    //if the time isn't passed in the response only display the date
-                    var eventDate = localEventTime == null ? localEventDate : dayjs(localEventDate + " " + localEventTime).format("MM/DD/YYYY  h:mm a");
-
-                    addEventList(newLinkRow, tagP, "", "col-span-8", id, "D", eventDate);
-
-                    //add the event venue(s)
-                    for (v = 0; v < data._embedded.events[i]._embedded.venues.length; v++) {
-                        addEventList(newBlockRow, tagP, "", "col-span-8", data._embedded.events[i].id, "V", mouseActionUnderline,
-                            mouseActionNone, "cursor: pointer", data._embedded.events[i]._embedded.venues[v].name);
-                    }
-                    for (v = 0; v < data._embedded.events[i]._embedded.venues.length; v++) {
-                        addEventList(newLinkRow, tagP, "", "col-span-8", id, "V", data._embedded.events[i]._embedded.venues[v].name);
-                    }
-
-                    $(newBlockRow).append(newLinkRow);
-                    $("#eventsList").append(newBlockRow);
-                    //Adds the url to the clickable row
-                    document.getElementById(id).href = data._embedded.events[i].url;
-                    //Opens the link in a new tab
-                    document.getElementById(id).target = "_blank";
-                }
-            }
-            else {
-                var newTag = $("<h1>")
-                newTag.attr("id", "no-events");
-                newTag.text("No events found");
-                $("#eventsList").prepend(newTag);
-        else
-{
-    newBlockRow = $("<h1>")
-    newBlockRow.attr("id", "no-events");
-    newBlockRow.text("No events found");
-    $("#eventsList").prepend(newBlockRow);
-}
-    }
-    catch (err) {
-    console.log(err);
-};
-
-};
-
-function addEventList(parentDiv, tagType, url, classType, id, idSuffix, mouseOver, mouseOut, cursorStyle, contentVal) {
-    function addEventList(parentDiv, tagType, url, classType, id, idSuffix, contentVal) {
-        var newTag = $(tagType);
-
-        if (tagType == "<img>") {
-            newTag.attr("id", id);
-            newTag.attr("src", url);
-            newTag.addClass(classType);
+    
+    try
+    {
+        //Dynamically add events to the list. The function takes: parent div, tag type, image source (if applicable), id, 
+        //id suffix, class, and text content.
+        //addEventList(parentDiv, tagType, imgSrc, id, idSuffix, classType, contentVal)
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        //max image size to display
+        var imgWidth = 205;
+        var imgHeight = 115;
+        var tagP = "<p>";
+        var newBlockRow;
+ 
+        if (window.screen.height <= 900)
+        {
+            //min image size to display
+            imgWidth = 100;
+            imgHeight = 56;
         }
-        else if (tagType == "<a>") {
-            parentDiv.href(url);
-        }
-        else {
-    else
+
+        //Skip adding html elements if no event returned
+        if (data.page.totalPages >> 0) 
+        {
+            for (var i = 0; i < data._embedded.events.length; i++) 
             {
-                newTag.attr("id", id + idSuffix);
-                if (!url == "") {
-                    newTag.attr("style", `background-image: url(\'${url}\')`)
+                var id = data._embedded.events[i].id;
+                newBlockRow = $("<div>")
+                //Add so row can be clickable.
+                var newLinkRow = $("<a>")
+                newLinkRow.attr("id", id);
+                
+                newLinkRow.addClass(`grid grid-cols-11 grid-rows-3 grid-flow-col gap-2 border-2 border-solid border-black`);
+                //add the event image. multiple images available so loop through to get correct size
+                for (var m = 0; m < data._embedded.events[i].images.length; m++)
+                {
+                    if (data._embedded.events[i].images[m].width == imgWidth && data._embedded.events[i].images[m].height == imgHeight)
+                    {
+                        addEventList(newLinkRow, '<div>', data._embedded.events[i].images[m].url, "row-span-3 col-span-1 max-w-full", "backgroundimage", "");
+                        break;
+                    }
                 }
-                newTag.addClass(classType);
-                newTag.text(contentVal);
-            }
 
-            parentDiv.append(newTag)
-            $("#eventsList").prepend(parentDiv);
-            if (!tagType == "<img>") {
-                $("#" + id + idSuffix).on("click", function () {
-                    console.log("Clicked event" + tagType + " " + id);
-                });
-            }
-        }
+                //add the event name
+                addEventList(newLinkRow, tagP, "", "col-span-8", id, "N", data._embedded.events[i].name);
 
-        // Record searches
-        /*
-        const searchOrigin = document.getElementById("originInput")
-        const searchInput = document.getElementById("destinationInput");
-        
-        
-        function recordSearch() {
-            const recentSearches = JSON.parse(localStorage.getItem("prevSearches")) || [];
-            const searchTerm = searchInput.value.trim(); // getting Going To Data
-            //const searchOriginTerm = searchOrigin.value.trim(); // getting Origin Data
-            const newPath = [searchOriginTerm, searchTerm]
-        
-            if (searchTerm !== "") {
-                recentSearches.unshift(newPath); // bring in the small arr into the main arr
-        
-                if (recentSearches.length > 5) {
-                    recentSearches.pop();
+                //add the event date
+                var localEventDate = dayjs(data._embedded.events[i].dates.start.localDate).format("MM/DD/YYYY");
+                var localEventTime = data._embedded.events[i].dates.start.localTime;
+                //if the time isn't passed in the response only display the date
+                var eventDate = localEventTime == null ? localEventDate : dayjs(localEventDate + " " + localEventTime).format("MM/DD/YYYY  h:mm a");
+
+                addEventList(newLinkRow, tagP, "", "col-span-8", id, "D", eventDate);
+
+                //add the event venue(s)
+                for (v = 0; v < data._embedded.events[i]._embedded.venues.length; v++)
+                {
+                    addEventList(newLinkRow, tagP, "", "col-span-8", id, "V", data._embedded.events[i]._embedded.venues[v].name);
                 }
-        
-                localStorage.setItem("prevSearches", JSON.stringify(recentSearches));
-        
-                displayRecentSearches();
-            }
-            searchOrigin.value = "";
-            searchInput.value = "";
-        }
-        */
-
-        // Function to get data store in local storage 
-        function checkLocalStorage() {
-            //get the data from local storage
-            var storedData = localStorage.getItem('prevSearches');
-            var dataArray = [];
-            if (storedData) {
-                //if there is data in local storage, trim the data then parse it into an array
-                storedData.trim();
-                dataArray = storedData.split(',');
-
-                //for each city in the array create a search link
-                for (var i = 0; i < dataArray.length; i++) {
-                    createRecentSearchLink(dataArray[i]);
-                }
+                
+                $(newBlockRow).append(newLinkRow);
+                $("#eventsList").append(newBlockRow);
+                //Adds the url to the clickable row
+                document.getElementById(id).href = data._embedded.events[i].url;
+                //Opens the link in a new tab
+                document.getElementById(id).target = "_blank";
             }
         }
-
-        // Function to Set data in Local storage
-        function saveToLocalStorage(city) {
-            event.preventDefault();
-            var data = localStorage.getItem('prevSearches');
-            if (data) {
-                //If there is data in local storage check if the just searched for city is already
-                //included in that data. If not, add the city to the list.
-                if (data.indexOf(city) === -1) {
-                    data = data + ',' + city;
-                    localStorage.setItem('prevSearches', data);
-                    createRecentSearchLink(city);
-                }
-            }
-            else {
-                //If there is no data in local storage, add it.
-                data = city;
-                localStorage.setItem('prevSearches', data);
-            }
+        else
+        {
+            newBlockRow = $("<h1>")
+            newBlockRow.attr("id", "no-events");
+            newBlockRow.text("No events found");
+            $("#eventsList").prepend(newBlockRow);
         }
+    }
+    catch (err) 
+    {
+        console.log(err);
+    };
+};
 
-        function createRecentSearchLink(city) {
-            var newLi = $("<li>")
-            var newP = $('<p>');
-            newP.attr('id', 'pastCity');
-            newP.attr("onmouseover", "this.style.textDecoration='underline'")
-            newP.attr("onmouseout", "this.style.textDecoration='none'")
-            newP.attr("style", "cursor: pointer")
-            newP.addClass("listSearches");
-            newP.text(city);
-            newLi.append(newP)
-            $("#prevSearches").prepend(newLi);
-            $("#pastCity").on("click", function () {
-                var newCity = $(this).text();
-                getCoordinates(newCity);
-                getEventsSearch(newCity);
-            });
+function addEventList(parentDiv, tagType, url, classType, id, idSuffix, contentVal)
+{
+    var newTag = $(tagType);
+
+    if (tagType == "<img>") 
+    {
+        newTag.attr("id", id);
+        newTag.attr("src", url);
+        newTag.addClass(classType);
+    }
+    else
+    {
+        newTag.attr("id", id + idSuffix);
+        if (!url == "") {
+            newTag.attr("style", `background-image: url(\'${url}\')`)
         }
+        newTag.addClass(classType);
+        newTag.text(contentVal);
+    }
 
-        init();
+    parentDiv.append(newTag)
+}
+
+// Function to get data store in local storage 
+function checkLocalStorage() 
+{
+    //get the data from local storage
+    var storedData = localStorage.getItem('prevSearches');
+    var dataArray = [];
+    if (storedData) 
+    {
+        //if there is data in local storage, trim the data then parse it into an array
+        storedData.trim();
+        dataArray = storedData.split(',');
+
+        //for each city in the array create a search link
+        for (var i = 0; i < dataArray.length; i++) {
+            createRecentSearchLink(dataArray[i]);
+        }
+    }
+}
+
+// Function to Set data in Local storage
+function saveToLocalStorage(city) {
+    event.preventDefault();
+    var data = localStorage.getItem('prevSearches');
+    if (data) {
+        //If there is data in local storage check if the just searched for city is already
+        //included in that data. If not, add the city to the list.
+        if (data.indexOf(city) === -1) {
+            data = data + ',' + city;
+            localStorage.setItem('prevSearches', data);
+            createRecentSearchLink(city);
+        }
+    }
+    else {
+        //If there is no data in local storage, add it.
+        data = city;
+        localStorage.setItem('prevSearches', data);
+    }
+}
+
+function createRecentSearchLink(city) {
+    var newLi = $("<li>")
+    var newP = $('<p>');
+    newP.attr('id', 'pastCity');
+    newP.attr("onmouseover", "this.style.textDecoration='underline'")
+    newP.attr("onmouseout", "this.style.textDecoration='none'")
+    newP.attr("style", "cursor: pointer")
+    newP.addClass("listSearches");
+    newP.text(city);
+    newLi.append(newP)
+    $("#prevSearches").prepend(newLi);
+    $("#pastCity").on("click", function () {
+        var newCity = $(this).text();
+        getCoordinates(newCity);
+        getEventsSearch(newCity);
+    });
+}
+
+init();
